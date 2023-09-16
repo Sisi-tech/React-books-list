e58d632b05e8e2eede5b5124fef1ea3e9360df00
sisi wang <sisiwang242@gmail.com>
2023-09-16 13:32:32 -0400

    first commit

---
+++ src/books.js
@@ -0,0 +1,21 @@
+export const books = [
+    {
+      img: "./images/Book1.jpg",
+      title: "The Art and Science of Getting Happier Hardcover",
+      author: "Arthur C. Brooks",
+      id: 1,
+    },
+    {
+      img: "./images/Book2.jpg",
+      title: "Fourth Wing (The Empyrean, 1)",
+      author: "Rebecca Yarros",
+      id: 2,
+    },
+    {
+        img: "./images/Book3.jpg",
+        title: "A Guide to Integrate and Transcend your Shadows",
+        author: "Keila Shaheen ",
+        id: 3,
+      }
+  ];
+
